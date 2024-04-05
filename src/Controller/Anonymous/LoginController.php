e06d24d8af00995f4580ba652a0cd4c5af4687f6
Gonzalez <gonzalezlouis1981@gmail.com>
2024-04-05 16:10:26 +0200

    FIX security login

--- src/Controller/Anonymous/LoginController.php
+++ src/Controller/Anonymous/LoginController.php
@@ -12,6 +12,11 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if($this->getUser()){
+            return $this->redirectToRoute("app_home");
+        }
+
+        dd($this->getUser());
          // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
