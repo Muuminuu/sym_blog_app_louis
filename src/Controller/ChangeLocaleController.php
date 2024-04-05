c9f96cbe8181be1a8a98d28d59170e3e8ac10bb6
Gonzalez <gonzalezlouis1981@gmail.com>
2024-04-05 15:13:38 +0200

    ADD change language

---
+++ src/Controller/ChangeLocaleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ChangeLocaleController extends AbstractController
+{
+    #[Route('/change_locale/{locale}', name: 'app_change_locale')]
+    public function changeLocale($locale, Request $request) 
+    {
+        // On stocke la langue dans la session
+        $request->getSession()->set('_locale', $locale);
+
+        // On revient sur la page précédente
+        return $this->redirectToRoute("app_home");
+    }
+}
